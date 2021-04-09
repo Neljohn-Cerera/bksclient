fc037a55051be896c6798c6907f96481d37ef04c
Neljohn R. Cerera <musicloverdisawake@gmail.com>
2021-04-09 21:28:14 +0800

    setting request records

--- src/components/Officials.js
+++ src/components/Officials.js
@@ -1,7 +1,121 @@
-function Officials() {
+import Card from "./officials/Card";
+
+function Officials({ docs }) {
   return (
     <>
-      <h1>Officials</h1>
+      {docs?.map((img) => {
+        if (img.rolee === "Barangay Image")
+          return (
+            <Card
+              key={img.id}
+              image={img.url}
+              role="Barangay Logo"
+              name="Bankas Heights"
+            />
+          );
+        if (img.rolee === "Punong Barangay")
+          return (
+            <Card
+              key={img.id}
+              image={img.url}
+              role="Punong Barangay"
+              name="Carmelita M. Babao"
+            />
+          );
+        if (img.rolee === "Barangay Treasurer")
+          return (
+            <Card
+              key={img.id}
+              image={img.url}
+              role="Barangay Treasurer"
+              name="Danilo O Omad"
+            />
+          );
+        if (img.rolee === "Barangay Secretary")
+          return (
+            <Card
+              key={img.id}
+              image={img.url}
+              role="Barangay Secretary"
+              name="Syril L. Nomus"
+            />
+          );
+        if (img.rolee === "SkChairman")
+          return (
+            <Card
+              key={img.id}
+              image={img.url}
+              role="SkChairman"
+              name="Angelou A. Nisnisan"
+            />
+          );
+
+        if (img.rolee === "Kagawad 1")
+          return (
+            <Card
+              key={img.id}
+              image={img.url}
+              role="Kagawad"
+              name="Ruel L. Betonio"
+            />
+          );
+        if (img.rolee === "Kagawad 2")
+          return (
+            <Card
+              key={img.id}
+              image={img.url}
+              role="Kagawad"
+              name="Elsie B. Amad"
+            />
+          );
+
+        if (img.rolee === "Kagawad 3")
+          return (
+            <Card
+              key={img.id}
+              image={img.url}
+              role="Kagawad"
+              name="Candido P. Borja Jr."
+            />
+          );
+        if (img.rolee === "Kagawad 4")
+          return (
+            <Card
+              key={img.id}
+              image={img.url}
+              role="Kagawad"
+              name="Efren R. Pamolino"
+            />
+          );
+        if (img.rolee === "Kagawad 5")
+          return (
+            <Card
+              key={img.id}
+              image={img.url}
+              role="Kagawad"
+              name="Angelo C. Gerondio"
+            />
+          );
+        if (img.rolee === "Kagawad 6")
+          return (
+            <Card
+              key={img.id}
+              image={img.url}
+              role="Kagawad"
+              name="Arvin L. Genabe"
+            />
+          );
+        if (img.rolee === "Kagawad 7")
+          return (
+            <Card
+              key={img.id}
+              image={img.url}
+              role="Kagawad"
+              name="Bernadette G. Al-ag"
+            />
+          );
+        return null;
+      })}
     </>
   );
 }
--- src/pages/OfficialsPage.js
+++ src/pages/OfficialsPage.js
@@ -1,15 +1,20 @@
 import Header from "../styledComponents/header";
 import Footer from "../styledComponents/footer";
 import Officials from "../components/Officials";
+import useFirestore from "../hooks/useFirestore";
+import officials from "../assets/css/officials/official.module.css";
 
 function OfficialsPage() {
+  const { docs } = useFirestore("images");
+  console.log("officilas docs", docs);
   return (
     <div>
       <header>
         <Header />
       </header>
-      <main>
-        <Officials />
+      <main className={officials.main}>
+        <h4>Barangay Officials</h4>
+        <Officials docs={docs} />
       </main>
       <footer>
         <Footer />
